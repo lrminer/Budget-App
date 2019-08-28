dc7a8f0a0181e980c465c79e24c379f7d4d55ac3
Logan Miner <loganrobertminer@gmail.com>
2019-08-28 10:39:28 -0400

    html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = function (app) {
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/index.html'))
+    });
+    app.get('/category', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/category.html'))
+    });
+    app.get('/compare', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/compare.html'))
+    });
+    app.get('/login', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/login.html'))
+    });
+    app.get('/newBudget', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/newBudget.html'))
+    });
+    app.get('/newUserInfo', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/newUserInfo.html'))
+    });
+    // app.get('/', function (req, res) {
+    //     res.sendFile(path.join(__dirname, '../public/index.html'))
+    // });
+}
