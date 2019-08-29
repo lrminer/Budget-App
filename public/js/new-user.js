6033a095296162757061c122d4d7adef625e3cac
Logan Miner <loganrobertminer@gmail.com>
2019-08-29 11:52:14 -0400

    updated new-user.js

--- public/js/new-user.js
+++ public/js/new-user.js
@@ -3,27 +3,21 @@ $(document).ready(function () {
     $('#submit-btn').on('click', function (event) {
         event.preventDefault();
 
-        const category1 = $('#category1').val();
-        const category2 = $('#category2').val();
-        const category3 = $('#category3').val();
-        const category4 = $('#category4').val();
-        const category5 = $('#category5').val();
-        const category6 = $('#category6').val();
-        const category7 = $('#category7').val();
-        const category8 = $('#category8').val();
-        const category9 = $('#category9').val();
+        const username = $('#input-username').val();
+        const password = $('#input-password').val();
+        const age = $('#input-age').val();
+        const location = $('#input-location').val();
+        const income = $('#input-income').val();
+
 
 
         const dataToSend = {
-            category1: category1,
-            category2: category2,
-            category3: category3,
-            category4: category4,
-            category5: category5,
-            category6: category6,
-            category7: category7,
-            category8: category8,
-            category9: category9,
+            username: username,
+            password: password,
+            age: age,
+            location: location,
+            income: income,
+
         };
 
         $.post('/api/budgets', dataToSend, function () {
