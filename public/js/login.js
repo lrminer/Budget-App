45536a199223abf8dfc242a812e597fa8b53616c
Logan Miner <loganrobertminer@gmail.com>
2019-08-29 13:42:15 -0400

    login js for username/pw

--- public/js/login.js
+++ public/js/login.js
@@ -3,29 +3,16 @@ $(document).ready(function () {
     $('#submit-btn').on('click', function (event) {
         event.preventDefault();
 
-        const category1 = $('#category1').val();
-        const category2 = $('#category2').val();
-        const category3 = $('#category3').val();
-        const category4 = $('#category4').val();
-        const category5 = $('#category5').val();
-        const category6 = $('#category6').val();
-        const category7 = $('#category7').val();
-        const category8 = $('#category8').val();
-        const category9 = $('#category9').val();
+        const username = $('#input-username').val();
+        const password = $('#input-password').val();
+
 
 
         const dataToSend = {
-            category1: category1,
-            category2: category2,
-            category3: category3,
-            category4: category4,
-            category5: category5,
-            category6: category6,
-            category7: category7,
-            category8: category8,
-            category9: category9,
+            username: username,
+            password: password
         };
-
+//// this will need to be changed to some authentication route
         $.post('/api/budgets', dataToSend, function () {
 
         }).then(function () {
