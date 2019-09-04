27bdb52072b66bdf7a82687344d789ef11b019f4
Logan Miner <loganrobertminer@gmail.com>
2019-09-04 15:24:07 -0400

    changed / html route to compare.html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/index.html'))
+        res.sendFile(path.join(__dirname, '../public/compare.html'))
     });
     app.get('/category', function (req, res) {
         res.sendFile(path.join(__dirname, '../public/category.html'))
