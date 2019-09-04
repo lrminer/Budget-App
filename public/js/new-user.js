256e48d0724aa9d3dad87a9fd85c4ef506262b51
Kevin Abram <htwnkid@gmail.com>
2019-09-04 10:44:44 -0400

    minor form changes to newUser form and javascript

--- public/js/new-user.js
+++ public/js/new-user.js
@@ -3,22 +3,22 @@ $(document).ready(function () {
     $('#submit-btn').on('click', function (event) {
         event.preventDefault();
 
-        const username = $('#input-username').val();
-        const password = $('#input-password').val();
+        const firstName = $('#input-first-name').val();
+        const lastName = $('#input-last-name').val();
         const age = $('#input-age').val();
         const location = $('#input-location').val();
         const income = $('#input-income').val();
 
 
         const dataToSend = {
-            username: username,
-            password: password,
-            age: age,
-            location: location,
-            income: income,
+            firstName: firstName,
+            lastName: lastName,
+            Age: age,
+            Location: location,
+            Income: income,
 
         };
-        
+
         console.log(dataToSend);
 
         $.post('/api/users', dataToSend, function () {
